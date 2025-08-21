b715df8d36d91caad0c0ca69ce81d903899e22f8
unknown <jaharri@ualberta.ca>
2025-08-21 12:19:50 -0600

    udpated launch directory

--- scripts/run_mmgbsa.py
+++ scripts/run_mmgbsa.py
@@ -13,50 +13,60 @@ def process_trajectory(traj_file):
     os.system(f'cpptraj -i {traj_file} -o md.dcd')
 
 
-def write_mmgbsa_job_file(dir):
-    with open('mmgbsa.job', 'w') as fp:
+def write_mmgbsa_job_file(md_dir):
+    job_file = os.path.join(md_dir, 'mmgbsa.job')
+    with open(job_file, 'w') as fp:
         fp.write(f"""#!/bin/bash
-    #SBATCH --time=00-12:00:00
-    #SBATCH --mem-per-cpu=8G
-    #SBATCH --cpus-per-task=1
-    #SBATCH --job-name={dir}_mmgbsa
-    #SBATCH --mail-type=ALL
-    #SBATCH --mail-user=jaharri1@ualberta.ca
-    #SBATCH --account=def-aminpour
+#SBATCH --time=00-12:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name={os.path.basename(os.path.dirname(md_dir))}_mmgbsa
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jaharri1@ualberta.ca
+#SBATCH --account=def-aminpour
 
-    module load {amber}
+module load StdEnv/2023 gcc/12.3 openmpi/4.1.5 cuda/12.2 amber-pmemd/24.3 ambertools/25.0
 
-    $AMBERHOME/bin/MMPBSA.py -O -i mmgbsa.in -o out1.dat -cp complex_stripped.prmtop -rp receptor.prmtop -lp ligand.prmtop -y md1.dcd
-    """)
-    
+$AMBERHOME/bin/MMPBSA.py -O -i mmgbsa.in -o out1.dat \\
+    -cp complex_stripped.prmtop \\
+    -rp receptor.prmtop \\
+    -lp ligand.prmtop \\
+    -y md1.dcd
+""")
+    return job_file
+
+
 def main():
     DIR = os.path.dirname(os.path.abspath(__file__))
     complex_dirs = [d for d in os.listdir('.') if os.path.isdir(d) and d.startswith('ternary')]
+
     for dir in complex_dirs:
         print(f'Processing directory: {dir}')
-        pull_files(DIR, 'traj.in', f'{dir}/md')
-        pull_files(DIR, 'mmgbsa.in', f'{dir}/md')
-        pull_files(f'{dir}/prep', 'complex_stripped.prmtop', f'{dir}/md')
-        pull_files(f'{dir}/prep', 'receptor.prmtop', f'{dir}/md')
-        pull_files(f'{dir}/prep', 'ligand.prmtop', f'{dir}/md')
+        md_dir = os.path.join(dir, "md")
 
-        os.chdir(f'{dir}/md')
+        # sanity check
+        if not os.path.isdir(md_dir):
+            print(f"Warning: {md_dir} does not exist, skipping...")
+            continue
 
-        process_trajectory('traj.in')
+        # copy inputs into md/
+        pull_files(DIR, 'traj.in', md_dir)
+        pull_files(DIR, 'mmgbsa.in', md_dir)
+        pull_files(os.path.join(dir, 'prep'), 'complex_stripped.prmtop', md_dir)
+        pull_files(os.path.join(dir, 'prep'), 'receptor.prmtop', md_dir)
+        pull_files(os.path.join(dir, 'prep'), 'ligand.prmtop', md_dir)
 
-        write_mmgbsa_job_file(DIR)
+        # change into md/, run everything, then go back
+        cwd = os.getcwd()
+        try:
+            os.chdir(md_dir)
 
-        os.system('sbatch mmgbsa.job')
+            process_trajectory('traj.in')
+
+            job_file = write_mmgbsa_job_file(md_dir)
+
+            os.system(f'sbatch {job_file}')
+        finally:
+            os.chdir(cwd)
 
 main()
-
-
-
-
-
-
-
-
-    
-
-
