ebfb647c43bea90ad9c144836401066345b40e0f
unknown <jaharri@ualberta.ca>
2025-08-26 11:44:10 -0600

    added run_mmgbsa.py to pipeline

--- shell/driver.sh
+++ shell/driver.sh
@@ -51,6 +51,11 @@ module load amber-pmemd/24.3
 md_jobid=$(sbatch --parsable --dependency=afterok:$dock_jobid --job-name=mdpy --output=mdpy.out --error=mdpy.err --wrap="python md.py")
 echo "Submitted md.py as job $md_jobid (after dock.py)"
 
+# === run_mmgbsa.py ===
+echo "Submitting run_mmgbsa.py to slurm after md_mmgbsa.py completes..."
+mmgbsa_jobid=$(sbatch --parsable --dependency=afterok:$md_jobid --job-name=mmgbsa --output=mmgbsa.out --error=mmgbsa.err --wrap="python run_mmgbsa.py")
+echo "Submitted run_mmgbsa.py as job $mmgbsa_jobid (after md.py)"
+
 # === analysis.py ===
 echo "Submitting analysis.py to SLURM after md.py completes..."
 analysis_jobid=$(sbatch --parsable --dependency=afterok:$md_jobid --job-name=analyzpy --output=analyzpy.out --error=analyzpy.err --wrap="python analysis.py")
