de56a6c723734428fbe3fea96251033a66786ddd
unknown <jaharri@ualberta.ca>
2025-08-27 12:38:31 -0600

    updated to remove md/docking in SLURM chain

--- shell/link_it.sh
+++ shell/link_it.sh
@@ -21,16 +21,18 @@ module load scipy-stack/2025a
 module load rdkit/2024.09.6
 module load python-build-bundle/2025b
 
+# === link_it.py ===
 source ~/reinvent4/bin/activate
 export PATH=$HOME/.local/bin:$PATH
-
-echo "PYTHON: $(which python)"
-echo "REINVENT: $(which reinvent)"
-echo "PATH: $PATH"
-echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
-env
 
 echo "Running REINVENT Link-INVENT sampling..."
 reinvent -l staged.log sampling.toml
 
 echo "Exit code: $?"
+
+link_jobid=$SLURM_JOB_ID
+
+# === dock.py ===
+echo "Submitting dock.py to SLURM after link_it.sh completes..."
+dock_jobid=$(sbatch --parsable --dependency=afterok:$link_jobid --job-name=dockpy --output=dockpy.out --error=dockpy.err --wrap="python dock.py")
+echo "Submitted dock.py as job $dock_jobid (after link_it.sh)"
